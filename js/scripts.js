ffc73b35abda6326ffcac391a8ee3a64877218cc
thomasz_05 <73875775+thomasz05@users.noreply.github.com>
2021-05-14 12:36:46 +0200

    bot

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,8 @@ $(document).on('click', function() {
     if (!localStorage.getItem('cookieconsent')) {
         document.body.innerHTML += '\
         <div id="cookieconsent">\
-            <i class="fas fa-exclamation-triangle"></i>\
             Questo sito fa uso di cookie per migliorare l\'esperienza di navigazione degli utenti, per presentare annunci pubblicitari personalizzati e per raccogliere informazioni sull\'utilizzo del sito stesso. Usando il sito accetti questo utilizzo. Consulta l\'<a href="../legal/cookie-policy.html" target="_blank">Informativa sui cookie</a>. \
-            <a href="#" id="cookieclose"><i class="fas fa-times"></i></a>\
+            <button type="button"id="cookieclose">Accetta</button></a>\
         </div>\
         ';
         $('#cookieclose').on('click', function(e) {
